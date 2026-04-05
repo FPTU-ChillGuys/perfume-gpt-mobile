ce4a9c54c378f36cc5c8d85d20e57dc7883df00e
xHexed <58384565+xHexed@users.noreply.github.com>
2026-04-05 11:19:10 +0700

    chore: delete unused files

--- packages/perfumegpt_common/bin/perfumegpt_common.dart
+++
@@ -1,5 +0,0 @@
-import 'package:perfumegpt_common/perfumegpt_common.dart' as perfumegpt_common;
-
-void main(List<String> arguments) {
-  print('Hello world: ${perfumegpt_common.calculate()}!');
-}
--- packages/perfumegpt_common/test/perfumegpt_common_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:perfumegpt_common/perfumegpt_common.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
