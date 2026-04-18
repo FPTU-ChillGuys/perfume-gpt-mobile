3d3b3a5d9d022038840ed7db3e469f6b6e8f6a55
xHexed <58384565+xHexed@users.noreply.github.com>
2026-04-18 16:33:19 +0700

    chore(staff_app): remove unused import

--- apps/staff_app/lib/features/pos/presentation/screens/batch_selection_dialog.dart
+++ apps/staff_app/lib/features/pos/presentation/screens/batch_selection_dialog.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
-import 'package:perfumegpt_api_client/perfumegpt_api_client.dart';
 import '../providers/counter_checkout_providers.dart';
 
 class BatchSelectionDialog extends ConsumerWidget {
